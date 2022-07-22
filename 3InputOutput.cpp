93e86e77bd0412f501f8492e9cf0c4c422ddf87a
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-07-22 23:09:34 +0600

    Create 3InputOutput.cpp

---
+++ 3InputOutput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+
+    // Printing something using cout << "";
+    cout << "Enter your age: ";
+
+    // Talking input by using cin >>
+    cin >> age;
+
+    cout << "Your age is " << age;
+
+    return 0;
+}
