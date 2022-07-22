04b1147ad50881841d9103be7114ad378fada96b
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-07-22 23:09:42 +0600

    Create 6IfElse.cpp

---
+++ 6IfElse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 17;
+
+    if (age >= 18)
+    {
+
+        cout << "Your are 18+";
+    }
+    else
+    {
+        cout << "You are not 18+";
+    }
+
+    return 0;
+}
