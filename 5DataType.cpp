a8c0b683269020f415576adb9f9ef080e3e8c643
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-07-22 23:09:40 +0600

    Create 5DataType.cpp

---
+++ 5DataType.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i = 5;
+    float f = 5.5;
+    double d = 110.00;
+    char c = 'a';
+    bool b = false;
+    
+    return 0;
+}
+
+// Learn more: https://www.programiz.com/cpp-programming/data-types
